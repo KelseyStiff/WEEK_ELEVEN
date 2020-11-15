1a54262ab4616b0e0ce72709359ba416c54b3ee8
kelseystiff <kelsey>
2020-11-15 13:37:32 -0600

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.json({'message': 'This is finally working!!'})
+    res.json({'message': 'is this working??????'})
 })
 
 module.exports = router
